a6d89ba1fde064e55b60367d0161debad5b80a08
LaurentDumont <ldumont@coldnorthadmin.com>
2018-04-15 10:59:26 -0400

    New role

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install the roles from galaxy
-ansible-galaxy install --force --role-file ovh-requirements.yml
+ansible-galaxy install --roles-path ./roles --force --role-file ovh-requirements.yml
 
 #launch the global playbook
 ansible-playbook -i ovh-inventory common-roles-playbook.yml --user=sysadmin --become --ask-pass --ask-become-pass
---
+++ scripts/create_ansible_folders.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p $1/tasks
+mkdir -p $1/vars
+mkdir -p $1/handlers
+touch $1/tasks/main.yml
+touch $1/vars/main.yml
+touch $1/handlers/main.yml
